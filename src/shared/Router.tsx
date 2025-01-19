36c66dce5cb4250c15c51caa069a5ba05d80309a
lim <dntksql1010@naver.com>
2025-01-19 18:39:42 +0900

    fix : 확장자 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Main from '../pages/Main';
-import Login from '../pages/Login';
+import Main from '../pages/Main.tsx';
+import Login from '../pages/Login.tsx';
 import { useAuthStore } from '../stroe/userStore';
 import { RouteProps } from '../types/type';
-import SignUp from '../pages/SignUp';
-import MyPage from '../pages/MyPage';
+import SignUp from '../pages/SignUp.tsx';
+import MyPage from '../pages/MyPage.tsx';
 
 const PrivateRoute: React.FC<RouteProps> = ({ element: Element, ...rest }) => {
   const { isAuthenticated } = useAuthStore();
