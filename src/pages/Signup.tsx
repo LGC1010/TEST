a4c05b20fe3238884d1e6b37bb5f1215be7ee6fd
lim <dntksql1010@naver.com>
2025-01-19 14:44:30 +0900

    add : 회원가입 로직 추가

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import { SetStateAction, useState } from 'react';
+import { useJoinMutation } from '../quries/useGetUserQuery';
 
-const Signup = () => {
-  return <div></div>;
+const SignUp = () => {
+  const [id, setId] = useState('');
+  const [password, setPassWord] = useState('');
+  const [nickname, setName] = useState('');
+  const { mutate } = useJoinMutation();
+
+  const handleId = (e: { target: { value: SetStateAction<string> } }) => {
+    setId(e.target.value);
+  };
+  const handlePassWord = (e: { target: { value: SetStateAction<string> } }) => {
+    setPassWord(e.target.value);
+  };
+  const handleName = (e: { target: { value: SetStateAction<string> } }) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div className='h-screen/90 flex justify-center items-center'>
+      <div className='min-h-96 rounded-3xl bg-white pt-12 pb-12 w-w/500 flex flex-col align items-center justify-center'>
+        <h2 className='text-4xl mb-4'>회원가입</h2>
+        <form
+          className=' bg-white pt-4 pb-4 w-w/500 flex flex-col align items-center justify-center'
+          onSubmit={(e) => {
+            e.preventDefault();
+            mutate({ id, password, nickname });
+          }}
+        >
+          <input
+            className='w-64 mb-4 rounded-3xl text-base text-center border-slate-400 py-2 px-2 border pd-2'
+            type='text'
+            value={id}
+            placeholder='아이디'
+            onChange={handleId}
+          />
+          <input
+            className='w-64 mb-4 rounded-3xl text-base text-center border-slate-400 py-2 px-2 border pd-2'
+            type='password'
+            value={password}
+            placeholder='비밀번호'
+            onChange={handlePassWord}
+          />
+          <input
+            className='w-64 mb-4 rounded-3xl text-base text-center border-slate-400 py-2 px-2 border pd-2'
+            type='text'
+            value={nickname}
+            placeholder='닉네임'
+            onChange={handleName}
+          />
+          <button className='w-40 mt-4 text-white bg-[#9cbfdb] py-3 px-4 border pd-2' type='submit'>
+            가입하기
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default Signup;
+export default SignUp;
--- src/quries/useGetUserQuery.ts
+++ src/quries/useGetUserQuery.ts
@@ -2,11 +2,12 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../stroe/userStore';
-type loginUser = { id: string; password: string };
+type LoginUser = { id: string; password: string };
+type User = { id: string; password: string; nickname: string };
 
 const API_URL = import.meta.env.VITE_API_URL;
 
-const logins = async (userData: loginUser) => {
+const loginRequest = async (userData: LoginUser) => {
   const response = await axios.post(`${API_URL}/login`, userData);
   return response.data;
 };
@@ -17,7 +18,7 @@ export const useLoginMutation = () => {
   const navigate = useNavigate();
 
   return useMutation({
-    mutationFn: logins,
+    mutationFn: loginRequest,
     onSuccess: (res) => {
       alert('로그인 성공');
       login(res.accessToken);
@@ -29,6 +30,29 @@ export const useLoginMutation = () => {
       console.log(error);
       console.log(`${import.meta.env.VITE_API_URL}/login`);
       console.log(`${API_URL}`);
+    }
+  });
+};
+
+const register = async (userData: User) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+export const useJoinMutation = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      alert('회원가입 성공');
+      navigate('/');
+      queryClient.invalidateQueries({});
+    },
+    onError: (error) => {
+      alert(error);
+      console.log(error);
     }
   });
 };
